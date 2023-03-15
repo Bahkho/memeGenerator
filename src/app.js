6c85c03fbf910fc81f9959638005650ed1769871
Abdulwahab Bakare <akoredebakare4u@gmail.com>
2023-03-15 15:46:35 -0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Meme from "./header";
+import Component from "./component";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <Meme />
+      <Component />
+    </div>
+  );
+}
